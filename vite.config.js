edc324dc0fb546a915f988243ae612c3e1d377d1
Abdul Majid <helloabdulmajid@gmail.com>
2026-09-20 16:15:25 +0530

    Prepare frontend for production deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-     allowedHosts: ['localui.abdulmajid.in', '.abdulmajid.in'],
     proxy: {
       "/api": "http://localhost:8080",
     },
